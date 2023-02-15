f897e1ded17e9a06b4ff991a7c3357f074aca792
husni <emhusni77@gmail.com>
2023-02-15 10:53:50 +0700

    index

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -10,9 +10,54 @@ import { withAuth } from "../../models/withAuthorization";
 import { Box, Container } from "@mui/system";
 import { useRouter } from 'next/router';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import HomeIcon from '@mui/icons-material/Home';
+
 import { ArrowBackIosNewOutlined } from "@mui/icons-material";
 import LogoutIcon from '@mui/icons-material/Logout';
+import { styled, alpha } from '@mui/material/styles';
 import { deleteCookie, getCookie } from "cookies-next";
+
+
+const StyledMenu = styled((props) => (
+  <Menu
+    elevation={0}
+    anchorOrigin={{
+      vertical: 'bottom',
+      horizontal: 'right',
+    }}
+    transformOrigin={{
+      vertical: 'top',
+      horizontal: 'right',
+    }}
+    {...props}
+  />
+))(({ theme }) => ({
+  '& .MuiPaper-root': {
+    borderRadius: 6,
+    marginTop: theme.spacing(1),
+    minWidth: 180,
+    color:
+      theme.palette.mode === 'light' ? 'rgb(55, 65, 81)' : theme.palette.grey[300],
+    boxShadow:
+      'rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px',
+    '& .MuiMenu-list': {
+      padding: '4px 0',
+    },
+    '& .MuiMenuItem-root': {
+      '& .MuiSvgIcon-root': {
+        fontSize: 18,
+        color: theme.palette.text.secondary,
+        marginRight: theme.spacing(1.5),
+      },
+      '&:active': {
+        backgroundColor: alpha(
+          theme.palette.primary.main,
+          theme.palette.action.selectedOpacity,
+        ),
+      },
+    },
+  },
+}));
 
 
 const Footer = () => {
@@ -65,9 +110,11 @@ function Layout({children}){
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
   };
+ 
   const handleClose = () => {
     setAnchorEl(null);
   };
+ 
 
   React.useEffect(() => {
     setUsername(JSON.parse(getCookie('user')).username.substring(0,1));
@@ -78,8 +125,10 @@ function Layout({children}){
     <AppBar position="static">
       <Toolbar>
         <Button color="inherit"  onClick={() => router.back()}><ArrowBackIosNewOutlined> </ArrowBackIosNewOutlined></Button>
-        <IconButton size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }} />
-        <MenuIcon />
+        {/* <IconButton size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 0.1 }} /> */}
+        <MenuIcon onClick={(e) => router.push("/")} />
+        
+
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>PT. Baramuda Bahari</Typography>
        
         <Tooltip title="Account">
